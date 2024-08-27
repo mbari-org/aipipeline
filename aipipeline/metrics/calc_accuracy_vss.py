d2050720ab279b5dfd4d2ed4d462d115b987270e
danellecline <dcline@mbari.org>
2024-08-26 17:31:04 -0700

    feat: added confusion matrix and more cleaning of metrics code

--- aipipeline/metrics/calc_accuracy_vss.py
+++ aipipeline/metrics/calc_accuracy_vss.py
@@ -1,14 +1,14 @@
 # aipipeline, Apache-2.0 license
 # Filename: aipipeline/metrics/calc_accuracy.py
 # Description: Calculate accuracy of models. Useful for measuring model performance, drift, etc.
+import os
+from datetime import datetime
 
-from pathlib import Path
-import redis
-from sklearn.metrics import precision_score, recall_score, accuracy_score
-from sklearn.preprocessing import LabelEncoder
+import dotenv
 import logging
 
 from aidata.predictors.process_vits import ViTWrapper
+from aipipeline.config_setup import setup_config
 
 logger = logging.getLogger(__name__)
 
@@ -16,59 +16,67 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 
+# Constants
+dotenv.load_dotenv()
+REDIS_PASSWD = os.getenv("REDIS_PASSWD")
+from pathlib import Path
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import precision_score, recall_score, accuracy_score
+import redis
 
-def calc_accuracy(config: dict, base_path: Path):
-    labels = config["data"]["labels"].strip().split(",")
+def calc_accuracy(config: dict, base_dir: str, password: str):
+    labels = []
+    labels_dir = Path(base_dir)
+    # Get all the labels from the directory names
+    for label in labels_dir.iterdir():
+        if label.is_dir():
+            labels.append(label.name)
+
+    base_path = Path(base_dir)
+    project = config["tator"]["project"]
     r = redis.Redis(
-        config["redis"]["host"],
-        config["redis"]["port"],
-        password=config["redis"]["password"],
+        host=config["redis"]["host"],
+        port=config["redis"]["port"],
+        password=password,
     )
     v = ViTWrapper(r, "cuda:0", False, 32)
 
     # Create a dictionary to encode the labels
     label_encoder = LabelEncoder()
-    label_encoder.fit(labels.split(","))
+    label_encoder.fit(labels)
     label_dict = {label: i for i, label in enumerate(label_encoder.classes_)}
 
-    LABELS_VAL = " ".join(labels)
+    LABELS_VAL = ", ".join(labels)
     true_labels = []
     predicted_labels = []
 
     for l in labels:
         image_root = base_path / l
-        image_paths = [str(image_path) for image_path in image_root.rglob("*.jpg")]
-        if len(image_paths) == 0:
-            print(f"No images found in {image_root}")
+        image_paths = list(image_root.rglob("*.jpg"))
+        if not image_paths:
+            logger.info(f"No images found in {image_root}")
             continue
 
-        predictions, scores = v.predict(image_paths, top_n=3)
+        # Predict the top 3 labels for each image
+        predictions, scores = v.predict([str(image_path) for image_path in image_paths], top_n=3)
         for i, prediction in enumerate(predictions):
-            class_name = image_paths[i].split("/")[-2]
+            class_name = image_paths[i].parent.name  # Correctly handle spaces in directory names
             predicted_labels.append(prediction)
             true_labels.append(class_name)
-
-    print(true_labels)
-    print(predicted_labels)
 
     true_labels_encoded = [label_dict[label] for label in true_labels]
 
     # Convert predictions to encoded labels
-    predicted_labels_encoded = []
-    for preds in predicted_labels:
-        encoded_preds = [label_dict[label] for label in preds]
-        predicted_labels_encoded.append(encoded_preds)
+    predicted_labels_encoded = [ [label_dict[label] for label in preds] for preds in predicted_labels ]
 
-    print(true_labels_encoded)
-    print(predicted_labels_encoded)
+    logger.info(true_labels_encoded)
+    logger.info(predicted_labels_encoded)
 
     # Calculate top-3 accuracy
     correct_predictions = sum(
-        [
-            1
-            for true_label, pred_labels in zip(true_labels_encoded, predicted_labels_encoded)
-            if true_label in pred_labels
-        ]
+        1
+        for true_label, pred_labels in zip(true_labels_encoded, predicted_labels_encoded)
+        if true_label in pred_labels
     )
     accuracy_top3 = correct_predictions / len(true_labels_encoded)
 
@@ -78,18 +86,44 @@ def calc_accuracy(config: dict, base_path: Path):
     recall = recall_score(true_labels_encoded, top1_predicted_labels_encoded, average="micro")
     accuracy_top1 = accuracy_score(true_labels_encoded, top1_predicted_labels_encoded)
 
-    print(f"{LABELS_VAL} Top-3 Accuracy: {accuracy_top3}")
-    print(f"{LABELS_VAL} Top-1 Accuracy: {accuracy_top1}")
-    print(f"{LABELS_VAL} Precision: {precision}")
-    print(f"{LABELS_VAL} Recall: {recall}")
+    logger.info(f"{LABELS_VAL} Top-3 Accuracy: {accuracy_top3}")
+    logger.info(f"{LABELS_VAL} Top-1 Accuracy: {accuracy_top1}")
+    logger.info(f"{LABELS_VAL} Precision: {precision}")
+    logger.info(f"{LABELS_VAL} Recall: {recall}")
 
+    # Save the confusion matrix
+    from sklearn.metrics import confusion_matrix
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import numpy as np
+    cm = confusion_matrix(true_labels_encoded, top1_predicted_labels_encoded)
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(cm, annot=True, fmt="d", xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_)
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.title("Confusion Matrix")
+    plt.suptitle(f"CM {project} exemplars")
+    d = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+    plt.title(d)
+    plt.savefig(f"confusion_matrix_{project}_{d}.png")
+
+
+def main(argv=None):
+    import argparse
+
+    example_project = Path(__file__).resolve().parent.parent / "projects" / "biodiversity" / "config" / "config.yml"
+    parser = argparse.ArgumentParser(description="Calculate the accuracy of the vss")
+    parser.add_argument("--config", required=False, help="Config file path", default=example_project)
+    parser.add_argument("--crops", required=True, help="Path to crops")
+    args = parser.parse_args(argv)
+
+    _, config_dict = setup_config(args.config)
+
+    if not os.getenv("REDIS_PASSWD"):
+        logger.error("REDIS_PASSWD environment variable is not set.")
+        return
+
+    calc_accuracy(config_dict, args.crops, REDIS_PASSWD)
 
 if __name__ == "__main__":
-    config_dict = {
-        "redis": {
-            "host": "localhost",
-            "port": 6379,
-            "password": None,
-        },
-    }
-    calc_accuracy(config_dict, Path("/mnt/UAV/Baseline_all/combined/crops/"))
+    main()
--- aipipeline/metrics/plot_tsne_vss.py
+++ aipipeline/metrics/plot_tsne_vss.py
@@ -1,15 +1,22 @@
 # aipipeline, Apache-2.0 license
 # Filename: aipipeline/metrics/plot_tsne_vss.py
 # Description: Batch process missions with visual search server classification
+import datetime
 import os
+import re
 import sys
 from datetime import datetime
 from pathlib import Path
 
-from config_setup import setup_config
+import numpy as np
+import redis
+from matplotlib import pyplot as plt
+from redis.commands.search.query import Query
+from sklearn.manifold import TSNE
+
+from aipipeline.config_setup import setup_config
 
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent.parent.parent))
-from aipipeline.metrics import gen_tsne_plot_vss
 import dotenv
 import logging
 
@@ -35,11 +42,121 @@ logger.addHandler(handler)
 dotenv.load_dotenv()
 REDIS_PASSWD = os.getenv("REDIS_PASSWD")
 
+def convert_redis_vector_to_float(vector_np):
+    return np.frombuffer(vector_np, dtype=np.float32)
+
+
+def extract_name(input_str):
+    match = re.match(r"^(.*?)(?=_[0-9]+)", input_str)
+    return match.group(1) if match else None
+
+
+def download_data(r: redis.Redis, page_size=50):
+    # Get all hashes from the index
+    query = Query("*").return_fields("id").return_field("vector", decode_field=False)
+    total_vectors = []
+    total_class_names = []
+    offset = 0
+    while True:
+        query.paging(offset, page_size)
+        results = r.ft("index").search(query)
+        vectors = [convert_redis_vector_to_float(result.vector) for result in results.docs]
+        names = [extract_name(result.id.split("doc:")[-1]) for result in results.docs]
+        total_vectors.extend(vectors)
+        total_class_names.extend(names)
+        if len(vectors) < page_size:
+            break
+        offset += page_size
+
+    return total_vectors, total_class_names
+
+
+def plot_tsne(config: dict, password: str):
+    try:
+        host = config["redis"]["host"]
+        port = config["redis"]["port"]
+        project = config["tator"]["project"]
+
+        logging.info(f"Connecting to Redis at {host}:{port}")
+        # Get the redis connection
+        r = redis.Redis(host, port, password=password)
+
+        vectors, class_names = download_data(r)
+        logging.info(f"Downloaded {len(vectors)} vectors from Redis")
+
+        if len(vectors) == 0:
+            logging.error("No vectors found in Redis. Exiting.")
+            return
+
+        tsne = TSNE(n_components=2, random_state=0)
+        class_idx = np.unique(class_names, return_inverse=True)[1]
+
+        final_vectors = []
+        for i in range(len(vectors)):
+            n = vectors[i] + [class_idx[i]]
+            final_vectors.append(n)
+
+        logging.info(f"Final vectors: {len(final_vectors)}")
+
+        v = np.array(final_vectors)
+        vectors_2d = tsne.fit_transform(v)
+        logging.info(f"t-SNE completed on {len(v)} vectors")
+
+        # Plot the t-SNE results, colored by class
+        plt.figure(figsize=(6, 5))
+
+        colors = [
+            (1, 0, 0),
+            (0, 1, 0),
+            (0, 0, 1),
+            (1, 1, 0),
+            (1, 0, 1),
+            (0, 1, 1),
+            (0.5, 0.5, 0),
+            (0.5, 0, 0.5),
+            (0, 0.5, 0.5),
+            (0.5, 0.5, 0.5),
+            (0.5, 0, 0),
+            (0, 0.5, 0),
+            (0, 0, 0.5),
+            (0.5, 0.5, 0),
+            (0.25, 0, 0.5),
+            (0, 0, 0),
+        ]
+
+        # Assign a color to each class, up to 16 classes then repeat
+        colors = colors * (len(np.unique(class_names)) // 16 + 1)
+
+        for i, class_name in enumerate(np.unique(class_names)):
+            plt.scatter([], [], c=colors[i], label=class_name, s=10)
+
+        # Adjust the plot to make room for the legend
+        plt.subplots_adjust(right=0.75)
+
+        # Customize and place the legend outside the plot
+        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5), title="Classes")
+        plt.suptitle(f"t-SNE of 768-dimensional vectors {project} exemplars")
+        d = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
+        plt.title(d)
+
+        # Plot the t-SNE results, colored by class
+        for i, class_name in enumerate(class_names):
+            x, y = vectors_2d[i]
+            idx = np.where(np.unique(class_names) == class_name)[0][0]
+            plt.scatter(x, y, c=colors[idx], label=class_name, s=10, alpha=0.5)
+
+        logging.info(f"Saving plot to tsne_plot_{project}_{d}.png")
+        plt.savefig(f"tsne_plot_{project}_{d}.png")
+        plt.show()
+    except Exception as e:
+        logging.exception(f"Error: {e}")
+        if "index: no such index" in str(e):
+            logging.info("Redis database not initialized.")
+
 
 def main(argv=None):
     import argparse
 
-    example_project = Path(__file__).resolve().parent.parent / "projects" / "uav" / "config" / "config.yml"
     example_project = Path(__file__).resolve().parent.parent / "projects" / "biodiversity" / "config" / "config.yml"
     parser = argparse.ArgumentParser(description="Reset the Vector Search Server (VSS) database.")
     parser.add_argument("--config", required=False, help="Config file path", default=example_project)
@@ -51,7 +168,7 @@ def main(argv=None):
         logger.error("REDIS_PASSWD environment variable is not set.")
         return
 
-    gen_tsne_plot_vss.plot_tsne(config_dict, REDIS_PASSWD)
+    plot_tsne(config_dict, REDIS_PASSWD)
 
 if __name__ == "__main__":
     main()
