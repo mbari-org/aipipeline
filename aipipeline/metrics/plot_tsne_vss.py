947ee8b6bace056cf5e33cc65cf9b785a32c72a5
danellecline <dcline@mbari.org>
2024-09-03 10:17:20 -0700

    chore: removed duplicate code

--- aipipeline/metrics/plot_tsne_vss.py
+++ aipipeline/metrics/plot_tsne_vss.py
@@ -22,8 +22,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
-
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 # Also log to the console
