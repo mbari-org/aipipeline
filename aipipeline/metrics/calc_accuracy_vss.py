36cb18710e05d848108194097d1800ba5b73ccd3
danellecline <dcline@mbari.org>
2024-09-03 10:05:41 -0700

    feat: add console log to vss accuracy, get class names from redis not config, and handle empty predictions

--- aipipeline/metrics/calc_accuracy_vss.py
+++ aipipeline/metrics/calc_accuracy_vss.py
@@ -4,19 +4,26 @@
 import glob
 import os
 from datetime import datetime
-
 import dotenv
 import logging
 import numpy as np
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from aidata.predictors.process_vits import ViTWrapper
 from aipipeline.config_setup import setup_config
 
 logger = logging.getLogger(__name__)
-
-logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+# and log to file
+now = datetime.now()
+log_filename = f"vss-calc-acc_vss_{now:%Y%m%d}.log"
+handler = logging.FileHandler(log_filename, mode="w")
+handler.setFormatter(formatter)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 # Constants
 dotenv.load_dotenv()
@@ -26,12 +33,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 import redis
 
-def calc_accuracy(config: dict, image_dir: str, password: str):
-    labels = config["data"]["labels"].split(",")
-    # If there are no labels, or the labels are "all", then we need to get the labels from the directory structure
-    if not labels or labels == ["all"]:
-        labels = [d.name for d in Path(image_dir).iterdir() if d.is_dir()]
 
+def calc_accuracy(config: dict, image_dir: str, password: str):
     base_path = Path(image_dir)
     project = config["tator"]["project"]
     r = redis.Redis(
@@ -40,6 +43,16 @@ def calc_accuracy(config: dict, image_dir: str, password: str):
         password=password,
     )
     v = ViTWrapper(r, "cuda:0", False, 32)
+
+    # Get all the class names and exemplar ids from the redis server
+    logger.info("Getting all class names and exemplar ids")
+    labels, ids = v.get_ids()
+
+    labels_unique = list(set(labels))
+    logger.info(f"Classes: {labels_unique}")
+    # Get all the classes that have only one exemplar
+    single_exemplar_classes = [c for c in labels_unique if labels.count(c) == 1]
+    logger.info(f"Single exemplar classes: {single_exemplar_classes}")
 
     # Create a dictionary to encode the labels
     label_encoder = LabelEncoder()
@@ -50,24 +63,33 @@ def calc_accuracy(config: dict, image_dir: str, password: str):
     true_labels = []
     predicted_labels = []
 
-    for l in labels:
+    for l in labels_unique:
+        logger.info(f"Processing {l}")
         image_root = base_path / l
         image_paths = list(image_root.rglob("*.jpg"))
+        image_ids = [int(image_path.stem) for image_path in image_paths]
         if not image_paths:
             logger.info(f"No images found in {image_root}")
             continue
 
         # Predict the top 3 labels for each image
-        predictions, scores = v.predict([str(image_path) for image_path in image_paths], top_n=3)
+        logger.info(f"Predicting {len(image_paths)} images")
+        predictions, scores, exemplar_ids = v.predict([str(image_path) for image_path in image_paths], top_n=3)
         for i, prediction in enumerate(predictions):
             class_name = image_paths[i].parent.name  # Correctly handle spaces in directory names
-            predicted_labels.append(prediction)
-            true_labels.append(class_name)
+            # Exclude exemplars from the accuracy calculation, except for classes with only one exemplar
+            if image_ids[i] not in ids or class_name in single_exemplar_classes:
+                predicted_labels.append(prediction)
+                true_labels.append(class_name)
+
+    if not true_labels:
+        logger.error(f"No images found to calculate accuracy in {base_path}")
+        return
 
     true_labels_encoded = [label_dict[label] for label in true_labels]
 
     # Convert predictions to encoded labels
-    predicted_labels_encoded = [ [label_dict[label] for label in preds] for preds in predicted_labels ]
+    predicted_labels_encoded = [[label_dict[label] for label in preds] for preds in predicted_labels]
 
     logger.info(true_labels_encoded)
     logger.info(predicted_labels_encoded)
@@ -92,21 +114,18 @@ def calc_accuracy(config: dict, image_dir: str, password: str):
     logger.info(f"{LABELS_VAL} Recall: {recall}")
 
     # Save the confusion matrix
-    from sklearn.metrics import confusion_matrix
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    # Normalize the encoded
     cm = confusion_matrix(true_labels_encoded, top1_predicted_labels_encoded)
     # Normalize the confusion matrix to range 0-1
     cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
     plt.figure(figsize=(12, 12))
-    sns.heatmap(cm_normalized, annot=True, fmt=".1f", xticklabels=label_encoder.classes_, yticklabels=label_encoder.classes_, cmap='Blues')
+    sns.heatmap(cm_normalized, annot=True, fmt=".1f", xticklabels=label_encoder.classes_,
+                yticklabels=label_encoder.classes_, cmap='Blues')
     plt.xlabel("Predicted")
     plt.ylabel("True")
     plt.title("Confusion Matrix")
-    plt.suptitle(f"CM {project} exemplars. Top-1 Accuracy: {accuracy_top1:.2f}, Top-3 Accuracy: {accuracy_top3:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}")
+    plt.suptitle(
+        f"CM {project} exemplars. Top-1 Accuracy: {accuracy_top1:.2f}, Top-3 Accuracy: {accuracy_top3:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}")
     d = f"{datetime.now():%Y-%m-%d %H:%M:%S}"
     plt.title(d)
     plot_name = f"confusion_matrix_{project}_{datetime.now():%Y-%m-%d %H%M%S}.png"
@@ -141,7 +160,7 @@ def main(argv=None):
         image_path = args.images
 
     # Remove any previous augmented data before starting
-    logger.info("Removing any previous augmented data")
+    logger.info("Removing augmented data")
     pattern = os.path.join(processed_data, '*.*.png')
     files = glob.glob(pattern)
     for file in files:
@@ -149,6 +168,7 @@ def main(argv=None):
         os.remove(file)
 
     calc_accuracy(config_dict, image_path, REDIS_PASSWD)
+
 
 if __name__ == "__main__":
     main()
