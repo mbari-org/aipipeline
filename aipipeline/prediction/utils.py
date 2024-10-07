93dc431a740a7c7e72a10fa3296c3fd55ff010ba
danellecline <dcline@mbari.org>
2024-10-07 10:58:13 -0700

    feat: added saliency computation and updated docs

--- aipipeline/prediction/utils.py
+++ aipipeline/prediction/utils.py
@@ -8,6 +8,8 @@ from datetime import datetime
 from pathlib import Path
 from typing import List
 
+import cv2
+import numpy as np
 from PIL import Image
 import pandas as pd
 
@@ -157,3 +159,191 @@ def top_majority(model_predictions, model_scores, threshold: float, majority_cou
     best_score /= num_majority
 
     return best_pred, best_score
+
+
+def compute_saliency(image, show=False):
+
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    img_lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+
+    # Blur the saturation channel to remove noise
+    img_hsv[:, :, 1] = cv2.GaussianBlur(img_hsv[:, :, 1], (15, 15), 0)
+
+    filtered_lum = img_lab[:, :, 0]
+    filtered_saturation = img_hsv[:, :, 1]
+
+    img_sl = cv2.merge((filtered_saturation, filtered_lum))
+
+    # Initialize an empty saliency map
+    saliency_map = np.zeros_like(image[:, :, 0], dtype=np.float32)
+
+    blurred_saturation = cv2.GaussianBlur(img_sl[:, :, 1], (5, 5), 0)
+    blurred_lum = cv2.GaussianBlur(img_sl[:, :, 1], (5, 5), 0)
+
+    # Calculate the center and surround regions for each channel
+    center_lum = cv2.GaussianBlur(blurred_lum, (3, 3), 2)
+    surround_lum = blurred_lum - center_lum
+
+    center_saturation = cv2.GaussianBlur(blurred_saturation, (3, 3), 2)
+    surround_saturation = blurred_saturation - center_saturation
+
+    # Normalize the values to the range [0, 255]
+    center_lum = cv2.normalize(center_lum, None, 0, 255, cv2.NORM_MINMAX)
+    surround_lum = cv2.normalize(surround_lum, None, 0, 255, cv2.NORM_MINMAX)
+
+    center_saturation = cv2.normalize(center_saturation, None, 0, 255, cv2.NORM_MINMAX)
+    surround_saturation = cv2.normalize(surround_saturation, None, 0, 255, cv2.NORM_MINMAX)
+
+    # Combine the center and surround regions for each channel
+    # Reduce the weight of the saturation channel
+    saliency_lum = 1.2*(center_lum - surround_lum)
+    saliency_saturation = (center_saturation - surround_saturation) / 4
+
+    # Combine the saliency maps into the final saliency map
+    saliency_level = (saliency_saturation + saliency_lum) / 2
+
+    # Resize the saliency map to the original image size
+    saliency_level = cv2.resize(saliency_level, (image.shape[1], image.shape[0]))
+
+    # Accumulate the saliency map at each level
+    saliency_map += saliency_level
+
+    # Normalize the final saliency map
+    saliency_map = cv2.normalize(saliency_map, None, 0, 255, cv2.NORM_MINMAX)
+
+    # Blur the saliency map to remove noise
+    saliency_map = cv2.GaussianBlur(saliency_map, (15, 15), 0)
+
+    if show:
+        # Display the original image and the saliency map
+        cv2.imshow('Original Image', image)
+        cv2.imshow('Fine-Grained Saliency Map', saliency_map.astype(np.uint8))
+        cv2.waitKey(0)
+
+    return saliency_map
+
+
+def compute_saliency_cost(contour, min_std: float, img_saliency: np.ndarray, img_luminance: np.ndarray) -> int:
+    """
+    Calculate the saliency cost of a contour. Lower saliency contours are more likely to be reflections.
+    :param contour: the contour
+    :param min_std: minimum standard deviation of the contour to be considered
+    :param img_saliency: saliency image
+    :param img_luminance: luminance image
+    :return: saliency cost (int)
+    """
+    # Calculate brightness and other statistics of the contour
+    mask = np.zeros_like(img_saliency)
+    cv2.drawContours(mask, [contour], 0, 255, thickness=cv2.FILLED)
+    mean_intensity_l = cv2.mean(img_luminance, mask=mask)[0]
+    mean_intensity_s = cv2.mean(img_saliency, mask=mask)[0]
+
+    # Calculate area of the contour
+    area = cv2.contourArea(contour)
+
+    # Calculate variance of pixel intensities within the contour
+    x, y, w, h = cv2.boundingRect(contour)
+    roi = img_luminance[y:y + h, x:x + w]
+    variance = np.var(roi)
+    std = np.std(roi)
+
+    # Create a factor to normalize the cost for different image sizes
+    factor = img_luminance.size / 10000
+
+    # The cost function penalizes smaller areas with low variance
+    cost = (area * (mean_intensity_l + mean_intensity_s + 0.1 * area) - variance) / factor
+
+    # If the std is too low, then the contour is not interesting; set the cost to 1
+    if std < min_std:
+        cost = 1
+
+    return int(cost)
+
+
+def extract_blobs(min_std: float, block_size: int, saliency_map: np.ndarray, img_color: np.ndarray, show=False) -> (pd.DataFrame, np.ndarray):
+    """
+    Extract blobs from a saliency map
+    :param min_std: minimum standard deviation of the contour to be considered
+    :param block_size: block size for adaptive thresholding
+    :param saliency_map: normalized saliency map
+    :param img_color: color image used when showing the results
+    :param show: True to show the results
+    :return: pandas dataframe of blobs, image with contours drawn
+    """
+
+    # Blur the saliency map to remove noise
+    saliency_map = cv2.GaussianBlur(saliency_map, (15, 15), 0)
+
+    # Threshold the saliency map using gradient thresholding
+    saliency_map_thres_c = cv2.adaptiveThreshold(
+        saliency_map.astype(np.uint8),
+        255,  # Max pixel value
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        block_size,  # Block size (size of the local neighborhood)
+        3  # Constant subtracted from the mean
+    )
+
+    # Invert the saliency map so that the salient regions are white
+    saliency_map_thres_c = cv2.bitwise_not(saliency_map_thres_c)
+
+    # Connect blobs by dilating then eroding them
+    # For images > 3000 pixels, use a 9x9 kernel, otherwise use a 5x5 kernel
+    width, height = saliency_map_thres_c.shape
+    logger.info(f'Saliency map image size: {width} x {height}')
+    if width > 3000 or height > 3000:
+        kernel1 = np.ones((5, 5), np.uint8)
+        kernel2 = np.ones((3, 3), np.uint8)
+        saliency_map_thres_c = cv2.dilate(saliency_map_thres_c, kernel1, iterations=4)
+        saliency_map_thres_c = cv2.erode(saliency_map_thres_c, kernel2, iterations=4)
+    else:
+        kernel1 = np.ones((4, 4), np.uint8)
+        kernel2 = np.ones((3, 3), np.uint8)
+        saliency_map_thres_c = cv2.dilate(saliency_map_thres_c, kernel1, iterations=1)
+        saliency_map_thres_c = cv2.erode(saliency_map_thres_c, kernel2, iterations=1)
+
+    contours, _ = cv2.findContours(saliency_map_thres_c, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
+
+    # Draw the contours on the image with a purple line border thickness of 2 px
+    contour_img = img_color.copy()
+    cv2.drawContours(contour_img, contours, -1, (81, 12, 51), 2)
+
+    # Get the luminance to use in saliency calculation
+    img_lum = cv2.cvtColor(img_color, cv2.COLOR_BGR2LAB)[:, :, 0]
+    df = process_contour(contours, min_std, saliency_map_thres_c.astype(np.uint8), img_lum)
+    return df, contour_img
+
+
+def process_contour(contours: np.ndarray, min_std: float, img_s: np.ndarray, img_l: np.ndarray) -> np.ndarray:
+    """
+    Process a single contour. Save the results to a csv file.
+    This is a separate function, so it can be run in parallel.
+    :param contours:  List of contours
+    :param min_std: minimum standard deviation of the contour to be considered
+    :param img_s: Saliency image
+    :param img_l: Luminance image
+    :return: dataframe of blobs
+    """
+    df = pd.DataFrame()
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+
+        area = cv2.contourArea(c)
+        saliency = compute_saliency(c, min_std, img_s, img_l)
+
+        logger.debug(f'Found blob area: {area}, saliency: {saliency}, area: {area}')
+        df = pd.concat([df, pd.DataFrame({
+            'image_path': '',
+            'class': 'Unknown',
+            'score': 0.,
+            'area': area,
+            'saliency': saliency,
+            'x': x,
+            'y': y,
+            'xx': x + w,
+            'xy': y + h,
+            'w': w,
+            'h': h,
+        }, index=[0])])
+
+    return df
