63237007002aacd2d9d0ad0c8f31b460fd8bc450
danellecline <dcline@mbari.org>
2024-10-22 14:05:56 -0700

    feat(uav): delete all loc in media

---
+++ aipipeline/projects/uav/delete_loc.py
@@ -0,0 +1,34 @@
+import os
+
+import tator
+import os
+
+kwargs = {}
+project_id = 4  # project in the database
+version_id = 25  # version in the database
+
+# Connect to Tator
+token = os.environ
+api = tator.get_api(host='http://mantis.shore.mbari.org', token='ae84628927851c1a545c375ea8e4c3da2a022400')
+
+# Search for media with name
+attribute_media = ["$name::trinity-2_20240820T233850_NewBrighton"]
+kwargs["attribute_contains"] = attribute_media
+medias = api.get_media_list(project=project_id, **kwargs)
+print(f"Found {len(medias)} media.")
+kwargs["version"] = [version_id]
+media_ids = [media.id for media in medias]
+batch_size = 1
+
+related_attr = [f"version::{version_id}"]
+
+for i in range(0, len(media_ids), batch_size):
+    print(f"Deleting localizations flagged for deletion for media index {i + batch_size} ...")
+    response = api.delete_localization_list(
+        project=project_id,
+        media_id=media_ids[i: i + batch_size],
+        related_attribute=related_attr
+    )
+    print(response)
+
+print(f"Finished deleting localizations flagged for deletion in project {project_id}")
