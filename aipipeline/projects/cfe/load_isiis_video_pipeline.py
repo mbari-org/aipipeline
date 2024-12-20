a7200ab690aaa2978b37b4e540bb021c0038635f
danellecline <dcline@mbari.org>
2024-12-20 12:16:27 -0800

    feat(cfe): video loading pipeline initial check-in

---
+++ aipipeline/projects/cfe/load_isiis_video_pipeline.py
@@ -0,0 +1,98 @@
+# aipipeline, Apache-2.0 license
+# Filename: projects/cfe/load_isiis_video_pipeline.py
+# Description: Batch load video for missions
+import os
+from datetime import datetime
+
+import apache_beam as beam
+import dotenv
+from apache_beam.options.pipeline_options import PipelineOptions
+import logging
+
+from aipipeline.docker.utils import run_docker
+from aipipeline.config_setup import setup_config
+from aipipeline.projects.uav.args_common import parse_args, POSSIBLE_PLATFORMS, parse_mission_string
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+now = datetime.now()
+log_filename = f"load-isiis-video{now:%Y%m%d}.log"
+handler = logging.FileHandler(log_filename, mode="w")
+handler.setFormatter(formatter)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+# Also log to the console
+console = logging.StreamHandler()
+logger.addHandler(console)
+logger.setLevel(logging.INFO)
+
+# Constants
+dotenv.load_dotenv()
+TATOR_TOKEN = os.getenv("TATOR_TOKEN")
+
+
+def load_video(element) -> str:
+    # Data is in the format
+    # <mission path>,<tator section>
+    # /mnt/CFElab/Data_archive/Videos/ISIIS/COOK/VideosMP4/20230712_RachelCarson,2023/07/RachelCarson
+    logger.info(f"Processing element {element}")
+    line, config_dict = element
+    mission_name, mission_dir, section = parse_mission_string(line)
+
+    if not mission_name:
+        logger.error(f"Could not find mission name in path: {line} that ends with {POSSIBLE_PLATFORMS}")
+        return f"Could not find mission name in path: {line} that ends with {POSSIBLE_PLATFORMS}"
+
+    logger.info(f"Mission name: {mission_name}")
+
+    project = config_dict["tator"]["project"]
+
+    logger.info(f"Loading videos in {mission_dir} to Tator project {project} in section {section}")
+    args = [
+        "load",
+        "video",
+        "--input",
+        mission_dir,
+        "--config",
+        f"/tmp/{project}/config.yml",
+        "--token",
+        TATOR_TOKEN,
+        "--section",
+        section,
+    ]
+
+    container = run_docker(
+        image=config_dict["docker"]["aidata"],
+        name=f"aidata-video-load-{mission_name}",
+        args_list=args,
+        bind_volumes=config_dict["docker"]["bind_volumes"]
+    )
+    if container:
+        logger.info(f"Video loading for {mission_name}...")
+        container.wait()
+        logger.info(f"Videos loaded for {mission_name}")
+        return f"Mission {mission_name} videos loaded."
+    else:
+        logger.error(f"Failed to load videos for {mission_name}")
+        return f"Failed to load Videos for {mission_name}"
+
+
+
+# Run the pipeline, reading missions from a file and skipping lines that start with #
+def run_pipeline(argv=None):
+    args, beam_args = parse_args(argv, logger)
+    options = PipelineOptions(beam_args)
+    conf_files, config_dict = setup_config(args.config)
+    with beam.Pipeline(options=options) as p:
+        (
+            p
+            | "Read missions" >> beam.io.ReadFromText(args.missions)
+            | "Filter comments" >> beam.Filter(lambda line: not line.startswith("#"))
+            | "Create elements" >> beam.Map(lambda line: (line, config_dict))
+            | "Process Missions (Load Videos)" >> beam.Map(load_video)
+            | "Log results" >> beam.Map(logger.info)
+        )
+
+
+if __name__ == "__main__":
+    run_pipeline()
