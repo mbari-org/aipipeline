6617ad1311e189e74b3ac7735addeb82daed91e5
danellecline <dcline@mbari.org>
2025-03-08 15:07:25 -0800

    chore(ptvr): added separate config for low and high mag and some refactoring in prep for model training

--- aipipeline/projects/planktivore/adjust_roi.py
+++ aipipeline/projects/planktivore/adjust_roi.py
@@ -1,17 +1,15 @@
 # Utility to read in all ROIS and pad using the long side of each ROI
-
+import argparse
 import cv2
 import os
 from pathlib import Path
 
-
-def pad_and_rescale(input_path:Path, output_path:Path):
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+def pad_and_rescale(input_path: Path, output_path: Path):
+    if not output_path.exists():
+        output_path.mkdir(parents=True, exist_ok=True)
 
     for roi_file in input_path.rglob("*.png"):
-
-        roi = cv2.imread(roi_file.as_posix())
+        roi = cv2.imread(str(roi_file))
         h, w, _ = roi.shape
         max_dim = max(h, w)
         pad_h = (max_dim - h) // 2
@@ -20,12 +18,12 @@ def pad_and_rescale(input_path:Path, output_path:Path):
         # Pad the ROI black, rescale and save
         roi_padded = cv2.copyMakeBorder(roi, pad_h, pad_h, pad_w, pad_w, cv2.BORDER_CONSTANT, value=[0, 0, 0])
         roi_padded_rescaled = cv2.resize(roi_padded, (224, 224))
-        cv2.imwrite(output_path / roi_file.name, roi_padded_rescaled)
-
+        cv2.imwrite(str(output_path / roi_file.name), roi_padded_rescaled)
 
 if __name__ == "__main__":
-    dataset = ["aidata-export-02-high-mag", "aidata-export-03-low-mag"]
-    for d in dataset:
-        input_dir = Path(f"/mnt/DeepSea-AI/data/Planktivore/raw/{d}")
-        output_dir = Path(f"/mnt/ML_SCRATCH/Planktivore/{d}-square")
-        pad_and_rescale(input_dir, output_dir)
+    parser = argparse.ArgumentParser(description="Pad and rescale ROIs to square format.")
+    parser.add_argument("input_dir", type=Path, help="Path to the input directory containing ROI images.")
+    parser.add_argument("output_dir", type=Path, help="Path to the output directory where processed images will be saved.")
+    args = parser.parse_args()
+
+    pad_and_rescale(args.input_dir, args.output_dir)
