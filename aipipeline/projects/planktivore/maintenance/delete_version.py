59f949db0510f9fbb89a3804749e85d10edf8eb9
danellecline <dcline@mbari.org>
2025-06-20 18:01:41 -0700

    refactor(ptvr): code reorg

--- aipipeline/projects/planktivore/maintenance/delete_version.py
+++ aipipeline/projects/planktivore/maintenance/delete_version.py
@@ -1,15 +1,15 @@
 import tator
 import os
 
-project_id = 12  # planktivore project in the database (mantis.shore.mbari.org)
-version_id = 61  # Version ID to delete localizations for
-
+project_id = 14  # planktivore project in the database (mantis.shore.mbari.org)
+version_id = 62  # Version ID to delete localizations for
+box_type = 18  # Box type ID for planktivore localizations (e.g., 17 for bounding boxes)
 # Connect to Tator
 token = os.getenv("TATOR_TOKEN")
 api = tator.get_api(host='https://mantis.shore.mbari.org', token=token)
 
 
-count = api.get_localization_count(project=project_id, type=17, version=[f"{version_id}"])
+count = api.get_localization_count(project=project_id, type=box_type, version=[f"{version_id}"])
 
 print(f"Found: {count} localizations for version {version_id}")
 if count > 0:
@@ -19,7 +19,7 @@ if count > 0:
     while count > 0:
         deleted = api.delete_localization_list(project=project_id, version=[version_id], start=0, stop=batch_size)
         print(f"Deleted {deleted if deleted is not None else 0} localizations in current batch.")
-        count = api.get_localization_count(project=project_id, type=17, version=[f"{version_id}"])
+        count = api.get_localization_count(project=project_id, type=box_type, version=[f"{version_id}"])
         if count == 0:
             print(f"All localizations for version {version_id} have been deleted.")
             break
