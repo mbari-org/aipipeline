e33e32b3768336b020137487dd50921363a9caff
danellecline <dcline@mbari.org>
2025-08-05 15:28:45 -0700

    refactor: refactoring for better batch processing compatible with latest fastapi-vss v0.10.2 VSS vector search system

---
+++ aipipeline/db/redis/vss/exemplars.py
@@ -0,0 +1,149 @@
+import logging
+import os
+from datetime import datetime
+from pathlib import Path
+from typing import List, Dict
+
+import dotenv
+
+from aipipeline.config_setup import CONFIG_KEY
+from aipipeline.engines.subproc import run_subprocess
+from aipipeline.prediction.library import gen_machine_friendly_label
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+# Also log to the console
+console = logging.StreamHandler()
+logger.addHandler(console)
+logger.setLevel(logging.INFO)
+# and log to file
+now = datetime.now()
+log_filename = f"vss_init_pipeline_{now:%Y%m%d}.log"
+handler = logging.FileHandler(log_filename, mode="w")
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+# Secrets
+dotenv.load_dotenv()
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+TATOR_TOKEN = os.getenv("TATOR_TOKEN")
+
+# Load exemplars into Vector Search Server
+def load_exemplars(labels: List[tuple[str, str]], conf_files=Dict) -> str:
+
+    logger.info(labels)
+    num_loaded = 0
+    for label, save_dir in labels:
+        machine_friendly_label = gen_machine_friendly_label(label)
+        # Grab the most recent file
+        all_exemplars = list(Path(save_dir).rglob("*_exemplars.csv"))
+        exemplar_file = sorted(all_exemplars, key=os.path.getmtime, reverse=True)[0] if all_exemplars else None
+
+        if exemplar_file is None:
+            logger.info(f"No exemplar file found for {label}")
+            exemplar_count = 0
+        else:
+            with open(exemplar_file, "r") as f:
+                exemplar_count = len(f.readlines())
+
+        if exemplar_count < 10 or exemplar_file is None:
+            all_detections = list(Path(save_dir).rglob("*_detections.csv"))
+            exemplar_file = sorted(all_detections, key=os.path.getmtime, reverse=True)[0] if all_detections else None
+
+            if exemplar_file is None:
+                logger.info(f"No detections file found for {label}")
+                continue
+
+            with open(exemplar_file, "r") as f:
+                exemplar_count = len(f.readlines())
+            logger.info(f"To few exemplars, using detections file {exemplar_file} instead")
+
+        logger.info(f"Loading {exemplar_count} exemplars for {label} as {machine_friendly_label} from {exemplar_file}")
+        args_list = [
+            "aidata",
+            "load",
+            "exemplars",
+            "--input",
+            f"'{exemplar_file}'",
+            "--label",
+            f"'{label}'",
+            "--device",
+            "cuda:0",
+            "--password",
+            REDIS_PASSWORD,
+            "--config",
+            conf_files[CONFIG_KEY],
+            "--token",
+            TATOR_TOKEN,
+        ]
+
+
+        try:
+            result = run_subprocess(args_list=args_list)
+            if result != 0:
+                logger.error(f"Error loading exemplars to VSS: {result}")
+                return f"Failed to load exemplars to VSS: {result}"
+            logger.info(f"Loaded cluster exemplars for {label} from {exemplar_file}")
+        except Exception as e:
+            logger.error(f"Failed to load v exemplars for {label}: {e}")
+            return f"Failed to load v exemplars for {label}: {e}"
+
+    return f"Loaded {num_loaded} labels"
+
+# Load exemplars into Vector Search Server
+def load_exemplar(data, conf_files=Dict) -> str:
+    logger.debug(data)
+    num_loaded = 0
+    _, label_path, save_dir = data
+    label = label_path.stem if isinstance(label_path, Path) else label_path
+    # Grab the most recent file
+    all_exemplars = list(Path(save_dir).rglob("*exemplars.csv"))
+    logger.info(f"Found {len(all_exemplars)} exemplar files for {label}")
+    exemplar_file = sorted(all_exemplars, key=os.path.getmtime, reverse=True)[0] if all_exemplars else None
+
+    if exemplar_file is None:
+        logger.info(f"No exemplar file found for {label}")
+        return f"No exemplar file found for {label}"
+
+    with open(exemplar_file, "r") as f:
+        exemplar_count = len(f.readlines())
+
+    logger.info(f"Loading {exemplar_count} exemplars for {label} as {label} from {exemplar_file}")
+    args_list = [
+        "aidata",
+        "load",
+        "exemplars",
+        "--input",
+        f"'{exemplar_file}'",
+        "--label",
+        f"'{label}'",
+        "--device",
+        "cuda:0",
+        "--password",
+        REDIS_PASSWORD,
+        "--config",
+        conf_files[CONFIG_KEY],
+        "--token",
+        TATOR_TOKEN,
+    ]
+    try:
+        result = run_subprocess(args_list=args_list)
+        if result != 0:
+            logger.error(f"Error loading examplars to VSS: {result}")
+            return f"Failed to load exemplars to VSS: {result}"
+        logger.info(f"Loaded cluster exemplars for {label} from {exemplar_file}")
+        num_loaded += 1
+    except Exception as e:
+        logger.error(f"Failed to load v exemplars for {label}: {e}")
+        return f"Failed to load exemplars for {label}: {e}"
+
+    return f"Loaded {num_loaded} labels"
+
+
+def load_exemplars(elements, conf_files: Dict) -> str:
+    logger.info(f"Loading {elements} ")
+    for element in elements:
+        load_exemplar(element, conf_files=conf_files)
+
+    return elements
--- aipipeline/db/redis/vss/init_pipeline.py
+++ aipipeline/db/redis/vss/init_pipeline.py
@@ -1,7 +1,6 @@
 # aipipeline, Apache-2.0 license
 # Filename: aipiipeline/db/redis/vss/init_pipeline.py
 # Description: Run the VSS initialization pipeline
-import time
 from datetime import datetime
 
 import dotenv
@@ -9,21 +8,20 @@ import os
 from pathlib import Path
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from typing import Dict
 import logging
 
 from aipipeline.config_args import parse_override_args
-from aipipeline.config_setup import extract_labels_config, setup_config, CONFIG_KEY
-from aipipeline.engines.subproc import run_subprocess
+from aipipeline.config_setup import extract_labels_config, setup_config
 from aipipeline.prediction.library import (
     download,
     compute_stats,
-    get_short_name,
-    gen_machine_friendly_label,
     clean,
-    batch_elements,
-    ProcessClusterBatch, remove_multicrop_views, clean_images, generate_multicrop_views,
+    remove_multicrop_views,
+    generate_multicrop_views,
+    clean_images,
+    cluster_collections
 )
+from aipipeline.db.redis.vss.exemplars import load_exemplars
 
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
@@ -45,60 +43,6 @@ REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 TATOR_TOKEN = os.getenv("TATOR_TOKEN")
 
 
-# Load exemplars into Vector Search Server
-def load_exemplars(data, config_dict=Dict, conf_files=Dict) -> str:
-    project = str(config_dict["tator"]["project"])
-    short_name = get_short_name(project)
-
-    logger.info(data)
-    num_loaded = 0
-    for label, save_dir in data:
-        machine_friendly_label = gen_machine_friendly_label(label)
-        # Grab the most recent file
-        all_exemplars = list(Path(save_dir).rglob("*exemplars.csv"))
-        logger.info(f"Found {len(all_exemplars)} exemplar files for {label}")
-        exemplar_file = sorted(all_exemplars, key=os.path.getmtime, reverse=True)[0] if all_exemplars else None
-
-        if exemplar_file is None:
-            logger.info(f"No exemplar file found for {label}")
-            return f"No exemplar file found for {label}"
-
-        exemplar_count = 0
-        with open(exemplar_file, "r") as f:
-            exemplar_count = len(f.readlines())
-
-        logger.info(f"Loading {exemplar_count} exemplars for {label} as {label} from {exemplar_file}")
-        args_list = [
-            "aidata",
-            "load",
-            "exemplars",
-            "--input",
-            f"'{exemplar_file}'",
-            "--label",
-            f"'{label}'",
-            "--device",
-            "cuda:0",
-            "--password",
-            REDIS_PASSWORD,
-            "--config",
-            conf_files[CONFIG_KEY],
-            "--token",
-            TATOR_TOKEN,
-        ]
-        try:
-            result = run_subprocess(args_list=args_list)
-            if result != 0:
-                logger.error(f"Error loading examplars to VSS: {result}")
-                return f"Failed to load exemplars to VSS: {result}"
-            logger.info(f"Loaded cluster exemplars for {label} from {exemplar_file}")
-            num_loaded += 1
-        except Exception as e:
-            logger.error(f"Failed to load v exemplars for {label}: {e}")
-            return f"Failed to load exemplars for {label}: {e}"
-
-    return f"Loaded {num_loaded} labels"
-
-
 # Beam pipeline
 def run_pipeline(argv=None):
     import argparse
@@ -108,11 +52,8 @@ def run_pipeline(argv=None):
     parser.add_argument("--config", required=True, help=f"Config file path, e.g. {example_project}")
     parser.add_argument("--clean", action="store_true", help="Clean previously downloaded data")
     parser.add_argument("--skip-download", required=False, default=False, help="Skip downloading data")
-    parser.add_argument("--batch-size", required=False, type=int, default=1, help="Batch size")
-    args, beam_args = parser.parse_known_args(argv)
 
     MIN_DETECTIONS = 2000
-    batch_size = int(args.batch_size)
     args, other_args = parser.parse_known_args(argv)
     options = PipelineOptions(other_args)
     conf_files, config_dict = setup_config(args.config, silent=True)
@@ -147,9 +88,8 @@ def run_pipeline(argv=None):
             | "Compute stats" >> beam.Map(compute_stats, config_dict=config_dict)
             | "Generate views" >> beam.Map(generate_multicrop_views)
             | "Clean bad examples" >> beam.Map(clean_images, config_dict=config_dict)
-            | 'Batch cluster ROI elements' >> beam.FlatMap(lambda x: batch_elements(x, batch_size=batch_size))
-            | 'Process cluster ROI batches' >> beam.ParDo(ProcessClusterBatch(config_dict=config_dict, min_detections=MIN_DETECTIONS))
-            | "Load exemplars" >> beam.Map(load_exemplars, config_dict=config_dict, conf_files=conf_files)
+            | "Cluster examples" >> beam.Map(cluster_collections, config_dict=config_dict, min_detections=MIN_DETECTIONS)
+            | "Load exemplars" >> beam.Map(load_exemplars, conf_files=conf_files)
             | "Log results" >> beam.Map(logger.info)
         )
 
