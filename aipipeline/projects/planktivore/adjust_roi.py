733b2d326666db029b4c6f52c3316514d08bd865
danellecline <dcline@mbari.org>
2025-08-26 11:24:35 -0700

    refactor(vss): refactoed pipeline to support compute_stats and crops from all pipelines

--- aipipeline/projects/planktivore/adjust_roi.py
+++ aipipeline/projects/planktivore/adjust_roi.py
@@ -1,4 +1,5 @@
 # Utility to read in all ROIS and pad using the long side of each ROI
+# Preserve parent directory structure
 import argparse
 import cv2
 from pathlib import Path
@@ -17,21 +18,30 @@ def process_roi(roi_file, output_path):
     pad_h = (max_dim - h) // 2
     pad_w = (max_dim - w) // 2
 
+    # Preserve only immediate parent directory
+    parent_dir = roi_file.parent.name
+    output_file = output_path / parent_dir / roi_file.name
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+
     # Pad the ROI black, rescale, and save
     roi_padded = cv2.copyMakeBorder(roi, pad_h, pad_h, pad_w, pad_w, cv2.BORDER_CONSTANT, value=[0, 0, 0])
     roi_padded_rescaled = cv2.resize(roi_padded, (224, 224))
-    cv2.imwrite(str(output_path / roi_file.name), roi_padded_rescaled)
+    cv2.imwrite(str(output_file), roi_padded_rescaled)
+
 
 def pad_and_rescale(input_path: Path, output_path: Path):
     if not output_path.exists():
         output_path.mkdir(parents=True, exist_ok=True)
 
     roi_files = list(input_path.rglob("*.png"))
+    roi_files += list(input_path.rglob("*.jpg"))
 
     num_workers = max(1, os.cpu_count() - 1)  # Use all but one CPU core
 
     with mp.Pool(processes=num_workers) as pool:
         pool.starmap(process_roi, [(roi_file, output_path) for roi_file in roi_files])
+
+    return output_path.as_posix()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pad and rescale ROIs to square format.")
--- aipipeline/projects/planktivore/init_vss_pipeline.py
+++ aipipeline/projects/planktivore/init_vss_pipeline.py
@@ -22,6 +22,7 @@ from aipipeline.prediction.library import (
     cluster_collections
 )
 from aipipeline.db.redis.vss.exemplars import load_exemplars
+from aipipeline.projects.planktivore.adjust_roi import pad_and_rescale
 
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
@@ -31,7 +32,7 @@ logger.addHandler(console)
 logger.setLevel(logging.DEBUG)
 # and log to file
 now = datetime.now()
-log_filename = f"vss_init_pipeline_{now:%Y%m%d}.log"
+log_filename = f"ptvr_init_vss_pipeline{now:%Y%m%d}.log"
 handler = logging.FileHandler(log_filename, mode="w")
 handler.setFormatter(formatter)
 handler.setLevel(logging.DEBUG)
@@ -47,7 +48,7 @@ TATOR_TOKEN = os.getenv("TATOR_TOKEN")
 def run_pipeline(argv=None):
     import argparse
 
-    parser = argparse.ArgumentParser(description="Initialize the VSS database")
+    parser = argparse.ArgumentParser(description="Initialize the VSS database with plankton data")
     example_project = Path(__file__).resolve().parent.parent / "projects" / "uav" / "config" / "config.yml"
     parser.add_argument("--config", required=True, help=f"Config file path, e.g. {example_project}")
     parser.add_argument("--clean", action="store_true", help="Clean previously downloaded data")
@@ -77,22 +78,29 @@ def run_pipeline(argv=None):
     download_args = config_dict["data"].get("download_args", []) # Get download arguments from config
     download_args = download_args.split(" ") if isinstance(download_args, str) else download_args # Convert to list if it's a string
     download_args = [arg for arg in download_args if arg] # Remove empty strings
-    download_args.extend(["--crop-roi", "--resize", "224"])
+    download_args.extend(["--crop-roi"])
     config_dict["data"]["download_args"] = download_args
+    download_path = Path(config_dict["data"]["download_dir"])
+    # Get the --version argument if it exists in the download args
     version_index = download_args.index("--version") if "--version" in download_args else -1
     versions = download_args[version_index + 1] if version_index != -1 and version_index + 1 < len(
         download_args) else "Baseline"
-    # The version path is a combined path of all versions specified, separated by underscores
+    # The version path created during download is a combined path of all versions specified, separated by underscores
     versions = versions.split(",")
     version_path = "_".join(versions)
     crop_path = download_path / version_path / "crops"
+
+    # Need to wrap this to handle the extra arg labels passed by beam from the "Download labeled data" step
+    def _pad_and_rescale(labels=[], input_path: Path=None, output_path: Path=None) -> str:
+        logger.info(f"Padding {input_path} to {output_path}")
+        return pad_and_rescale(input_path=input_path, output_path=output_path)
 
     with beam.Pipeline(options=options) as p:
         (
             p
             | "Start download" >> beam.Create([labels])
             | "Download labeled data" >> beam.Map(download, conf_files=conf_files, config_dict=config_dict)
-            | "Start stats" >> beam.Create([crop_path.as_posix()])
+            | "Square ROIs" >> beam.Map(_pad_and_rescale, input_path=crop_path, output_path=crop_path)
             | "Compute stats" >> beam.Map(compute_stats)
             | "Generate views" >> beam.Map(generate_multicrop_views)
             | "Clean bad examples" >> beam.Map(clean_images, config_dict=config_dict)
