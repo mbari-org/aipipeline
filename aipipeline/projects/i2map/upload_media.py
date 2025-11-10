8979de395e411e66b9bdddfccabe0dab8888fd9a
danellecline <dcline@mbari.org>
2025-11-09 19:44:58 -0800

    feat(i2map): upload video to i2map.shore

---
+++ aipipeline/projects/i2map/upload_media.py
@@ -0,0 +1,45 @@
+# Utility script to load i2MAP video into Tator
+# Run with python upload_media.py --host http://i2map.shore.mbari.org --token <token> --type-id <type_id> --media-path <path_to_media>
+import logging
+import sys
+import time
+
+import tator
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    # Create a parser that includes path to media file.
+    parser = tator.get_parser()
+    parser.add_argument("--type-id", help="Media type ID for upload.", required=True, type=int)
+    parser.add_argument(
+        "--chunk-size",
+        help="The maximum chunk size for uploads.",
+        required=False,
+        type=int,
+        default=10 * 1024 * 1024,
+    )
+    parser.add_argument("--media-path", help="Path to media file.", required=True)
+    args = parser.parse_args()
+
+    # Create the api.
+    tator_api = tator.get_api(args.host, args.token)
+
+    # Upload the media.
+    for progress, response in tator.util.upload_media(
+        api=tator_api, type_id=args.type_id, path=args.media_path, chunk_size=args.chunk_size
+    ):
+        logger.info(f"Upload progress: {progress}%")
+    logger.info(response.message)
+
+    # Take a look at transcode progress, wait until complete.
+    project = tator_api.get_media_type(args.type_id).project
+    while True:
+        transcode = tator_api.get_transcode(response.id)
+        status = transcode.job.status.lower()
+        if status == "succeeded":
+            break
+        elif status == "failed":
+            raise ValueError("Upload failed!")
+        time.sleep(10)
