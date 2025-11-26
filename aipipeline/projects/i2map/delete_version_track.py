a50a8218fde706910f924eedb63cc235c06016df
danellecline <dcline@mbari.org>
2025-11-26 09:52:08 -0800

    feat(i2map): add version delete_version_track.py

---
+++ aipipeline/projects/i2map/delete_version_track.py
@@ -0,0 +1,41 @@
+import tator
+import os
+
+project_id = 3  # bio project in the database (mantis.shore.mbari.org)
+version_id = 71  # Version ID to delete localizations for
+box_type = 3  # Box type ID
+# Connect to Tator
+token = os.getenv("TATOR_TOKEN")
+api = tator.get_api(host='https://mantis.shore.mbari.org', token=token)
+
+count = api.get_localization_count(project=project_id, type=box_type, version=[f"{version_id}"])
+
+print(f"Found: {count} localizations for version {version_id}")
+if count > 0:
+
+    # Delete in batches
+    batch_size = 1000
+    while count > 0:
+        deleted = api.delete_localization_list(project=project_id, version=[version_id], start=0, stop=batch_size)
+        print(f"Deleted {deleted if deleted is not None else 0} localizations in current batch.")
+        count = api.get_localization_count(project=project_id, type=box_type, version=[f"{version_id}"])
+        if count == 0:
+            print(f"All localizations for version {version_id} have been deleted.")
+            break
+
+# Delete all the states in this version
+state_count = api.get_state_count(project=project_id, version=[f"{version_id}"])
+print(f"Found: {state_count} states for version {version_id}")
+if state_count > 0:
+    # Delete in batches
+    batch_size = 1000
+    while state_count > 0:
+        deleted_states = api.delete_state_list(project=project_id, version=[version_id], start=0, stop=batch_size)
+        print(f"Deleted {deleted_states if deleted_states is not None else 0} states in current batch.")
+        state_count = api.get_state_count(project=project_id, version=[f"{version_id}"])
+        if state_count == 0:
+            print(f"All states for version {version_id} have been deleted.")
+            break
+
+# Delete the version itself
+# api.delete_version(id=version_id)
